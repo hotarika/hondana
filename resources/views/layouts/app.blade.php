916f7980b5719e54570e480e7c1e46fdc04642b3
tatata <mugicha8932@gmail.com>
2021-01-22 07:35:17 +0900

    refactor: header, footer コンポーネント化
    上記に加え、headerのユーザーネーム部分を動的に変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,58 +5,8 @@
 
 <body>
     <div id="app">
-        <header class="l-header">
-            <div class="container">
-                <div class="p-header">
-                    {{-- logo --}}
-                    <div class="p-header__logoWrap">
-                        <a @guest href="/"
-                            @else href="{{route('home')}}"
-                            @endguest>
-                            <img class="p-header__logoIcon" src="{{asset('icon/logo/logoIcon.svg')}}" alt="ロゴアイコン">
-                            <img class="p-header__logo" src="{{asset('icon/logo/logo.svg')}}"
-                                alt="ロゴ">
-                        </a>
-                    </div>
-
-                    @guest {{-- ゲストユーザーの場合 --}}
-
-                    <nav class="p-header__nav">
-                        <ul class="p-header__navLists">
-                            <li class="p-header__navList -login">
-                                <a href="{{route('login')}}">ログイン</a>
-                            </li>
-                            <li class="p-header__navList -register">
-                                <a href="{{route('register')}}">会員登録</a>
-                            </li>
-                        </ul>
-                    </nav>
-
-                    @else {{-- ログインユーザーの場合 --}}
-
-                    {{-- navigation --}}
-                    <nav class="p-header__nav js-nav">
-                        <i class="far fa-times-circle p-header__menuClose js-closeIcon"></i>
-                        <ul class="p-header__navLists">
-                            <li class="p-header__navList -name">ここに名前が入ります</li>
-                            <li class="p-header__navList -myShelf">
-                                <a href=""><i class="fas fa-smile"></i>わたしの本棚</a>
-                            </li>
-                            <li class="p-header__navList -bookRegister">
-                                <a href=""><i class="fas fa-book"></i>本棚に登録</a>
-                            </li>
-                            <li class="p-header__navList -logout">
-                                <a href=""><i class="fas fa-sign-out-alt"></i>ログアウト</a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <img class="p-header__menuOpen js-openIcon" src="{{asset('icon/bars-solid.svg')}}"
-                        alt="ハンバーガーメニュー" />
-                    @endguest
-
-                </div>
-            </div>
-        </header>
+        {{-- header --}}
+        @include('components/header')
 
         <main class="l-main">
             <div class="container">
@@ -64,11 +14,8 @@
             </div>
         </main>
 
-        <footer class="l-footer">
-            <img class="l-footer__logoIcon" src="{{asset('icon/logo/logoIcon.svg')}}" alt="ロゴアイコン">
-            <img class="l-footer__logo" src="{{asset('icon/logo/logo.svg')}}"
-                alt="ロゴ">
-        </footer>
+        {{-- footer --}}
+        @include('components/footer')
     </div>
 </body>
 
